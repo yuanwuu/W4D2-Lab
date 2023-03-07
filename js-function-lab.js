4d19831e3406eb5e6efcd74e71f1cee476279d21
Yuan Wu <wu.yuanf@gmail.com>
2023-03-07 17:15:19 -0500

    completed

--- js-function-lab.js
+++ js-function-lab.js
@@ -9,32 +9,14 @@ console.log(minusOne(10))
 console.log(minusOne(100))
 console.log(minusOne('Infinity'))
 
+console.log(`----------------------------------`)
 
 function makeSentence (param1,param2,param3) {
     return `Oh boy, do ${param1} ${param2} ${param3} or what?`
 }
 console.log(makeSentence('I', 'want', 'chimichangas'))
 
-
-
-/*
-map() creates a new array from calling a function for every array element.
-
-map() calls a function once for each element in an array.
-
-map() does not execute the function for empty elements.
-
-map() does not change the original array.
-
-
-const num = [65, 44, 12, 4];
-const newArr = num.map(myFunction)
-
-function myFunction(num) {
-  return num * 10;
-}
-console.log(newArr)
-*/
+console.log(`------------------------------------`)
 
 function getLastElement (arr) {
     return arr[arr.length - 1];

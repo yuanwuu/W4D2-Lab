250984e4b91fef041c2a57c450345fedef6cabe2
Yuan Wu <wu.yuanf@gmail.com>
2023-03-07 16:57:50 -0500

    due 3/7 at midnight

--- js-function-lab.js
+++ js-function-lab.js
@@ -13,43 +13,7 @@ function minusOne(num){
 function makeSentence (param1,param2,param3) {
     return `Oh boy, do ${param1} ${param2} ${param3} or what?`
 }
-//console.log(makeSentence('I', 'want', 'chimichangas'))
-
-
-// function getLastElement (arr) {
-
-// }
-// getLastElement([1, 2, 3, 4, 5, 6]);       // 6
-// getLastElement(['a', 'b', 'c']);          // 'c'
-// getLastElement([[1, 2, 3], [4, 5, 6]]);   // [4, 5, 6]
-
-const numbers = [1, 2, 3, 4, 5]; 
- 
-const bigNumbers = numbers.map(number => {
-  return number * 1;
-});
-
-
-//console.log(bigNumbers)
-
-
-// const finalParticipants = ['Taylor', 'Donald', 'Don', 'Natasha', 'Bobby'];
-
-// // add string after each final participant
-// const announcements = finalParticipants.map(mem => {
-//   return mem + ' joined the contest.';
-// })
-
-// console.log(announcements);
-
-
-/**
- * .find()
- * .map()
- * 
- */
-
-
+console.log(makeSentence('I', 'want', 'chimichangas'))
 
 
 
